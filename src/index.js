a8eae3b6898bbee91bdfb32bb99a7740719d76ef
SaieeashReddy23 <saieeashreddy1@gmail.com>
2021-09-30 00:08:42 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const image = "https://images-eu.ssl-images-amazon.com/images/I/71GYH1FTilL._AC_UL200_SR200,200_.jpg ";
+const title = "My Life in Full: Work, Family, and Our Future ";
+const author = "Indra Nooyi";
+
+const books = [
+  {
+    image : "https://images-eu.ssl-images-amazon.com/images/I/71GYH1FTilL._AC_UL200_SR200,200_.jpg ",
+    title : "My Life in Full: Work, Family, and Our Future ",
+    author : "Indra Nooyi"
+  }
+  ,
+   {
+    image : "https://images-eu.ssl-images-amazon.com/images/I/51u2E5fNq8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title : "Rich Dad Poor Dad ",
+    author : "Robert Kiyosaki"
+  }
+  ,
+  {
+    image : "https://images-eu.ssl-images-amazon.com/images/I/61MrRA4qE0L._AC_UL200_SR200,200_.jpg",
+    title : "Polity for civil services",
+    author : "M.Laxmikanth"
+  }
+]
+ 
+
+function BookList(){
+  return (
+   <section className="bookList">
+    
+    {books.map((book) =>{
+      return (
+        <Book book={book}/> 
+      );
+    })}
+   </section>
+  );
+}
+
+const Book = (props) => {
+
+  const {image,title,author} = props.book;
+  return(
+     <article className = "book">
+         <img src={image} alt="Indra nooyi biography"/>
+         <h1>{title}</h1>
+         <h4>{author}</h4>
+     </article>
+  );
+};
+
+
+ReactDOM.render(<BookList/>,document.getElementById('root'));
